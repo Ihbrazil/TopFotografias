9bdfaa4447bc8ed4f23afa06e116db5f88dc557e
ihbrazil <ihbrazil@users.noreply.github.com>
2024-11-30 20:26:19 -0300

    Atualização da 4ª Missão da Carreira Programador Mobile - Containers
    4ª Missão da Carreira Programador Mobile - Containers - Componente ImageBackground - Exemplo prático - utilizando três cards com diferentes imagens de fundo.

---
+++ estilos.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from "react-native";
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20
+  },
+ 
+  titulo: {
+    fontSize: 26,
+    fontWeight: "bold",
+    marginBottom: 15,
+    textAlign: "center",
+  },
+
+  subtitulo:{
+    fontSize: 16,
+    fontWeight: "normal",
+    textAlign: "center",
+  },
+
+  card: {
+    width: 300,
+    height: 150,
+    marginTop: 30,
+  },
+  
+  cardTitulo: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingTop: 10
+  },
+
+  cardParagrafo: {
+    color: '#fff',
+    fontWeight: "normal",
+    textAlign: "center",
+  }
+});
+
+export default estilos;
